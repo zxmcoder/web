6119e7fb9fa09924a32e4b8b4d607ce9edba3fea
zxmcoder <zxmcoder@163.com>
2022-01-23 16:30:17 +0800

    write playground 2

---
+++ game/static/js/src/playground/fireball/zbase.js
@@ -0,0 +1,44 @@
+
+class FireBall extends AcGameObject {
+    constructor(playground, player, x, y, radius, vx, vy, color, speed, move_length, damage) {
+        super();
+
+        this.playground = playground;
+        this.player = player;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.move_length = move_length;
+        this.damage = damage;
+        this.eps = 0.1;
+    }
+
+    start() {
+
+    }
+
+    update() {
+
+    }
+
+    get_dist() {
+
+    }
+
+    is_collision() {
+
+    }
+
+    attack() {
+
+    }
+
+    render() {
+        
+    }
+}
---
+++ game/static/js/src/playground/player/zbase.js
@@ -0,0 +1,57 @@
+
+class Player extends AcGameObject {
+    constructor(playground, x, y, radius, color, speed, is_me) {
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.damage_x = 0;
+        this.damage_y = 0;
+        this.damage_speed = 0;
+        this.move_length = 0;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.eps = 0.1;
+        this.friction = 0.9;
+        this.spent_time = 0;
+
+        this.cur_skill = null;
+    }
+
+    start() {
+
+    }
+
+    add_listening_events() {
+
+    }
+
+    shoot_fireball() {
+
+    }
+
+    get_dist() {
+
+    }
+
+    move_to() {
+
+    }
+
+    is_attacked() {
+
+    }
+
+    update() {
+
+    }
+
+    render() {
+        
+    }
+}
